7376e8f476b884295ede59aa4b83342074c080ce
Hugo59530 <hugo.houdain2@gmail.com>
2026-03-18 16:30:51 +0100

    v2

--- argo.sh
+++ argo.sh
@@ -1,3 +1,4 @@
 kubectl create namespace argocd
 kubectl apply -n argocd --server-side --force-conflicts -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+kubectl apply -f argocd.yaml
